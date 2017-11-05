36e384ea82f214c45b11cdf50e0ccde1653a9a2e
Emma Fahlström <emma_s_f@hotmail.com>
2017-11-05 16:02:38 +0100

    Added compositeEdge and GraphReducer

---
+++ WindowsFormsApp1/GraphReducer.cs
@@ -0,0 +1,58 @@
+﻿using QuickGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class GraphReducer<TVertex>
+    {
+        public static Graph<TVertex, Edge<TVertex>> CreateReducedGraph(Graph<TVertex, Edge<TVertex>> graph)
+        {
+            var reducedGraph = new Graph<TVertex, Edge<TVertex>>();
+
+            HashSet<TVertex> bonusStartDestSet = new HashSet<TVertex>(); // bonusStartDestSet = C
+            // C = B + {s, d}
+            foreach(var v in graph.BonusVertices.Keys)
+            {
+                bonusStartDestSet.Add(v);
+            }
+            bonusStartDestSet.Add(graph.Start);
+            bonusStartDestSet.Add(graph.Goal);
+
+            foreach (var source in bonusStartDestSet)
+            {
+                reducedGraph.AddVertex(source);
+                TryFunc<TVertex, IEnumerable<Edge<TVertex>>> tryGetPaths = graph.getAllShortestPath(source);
+                foreach (var destination in bonusStartDestSet)
+                {
+                    if (source.Equals(destination))
+                        continue;
+
+                    IEnumerable<Edge<TVertex>> path;
+                    double cost = 0;
+                    if (tryGetPaths(destination, out path))
+                    {
+                        foreach (var e in path)
+                        {
+                            cost += graph.EdgeCosts[e];
+                        }
+
+                        if (graph.BonusVertices.ContainsKey(destination))
+                            cost -= graph.BonusVertices[destination];
+
+                        var compositeEdge = new CompositeEdge<TVertex>(source, destination, path);
+                        reducedGraph.AddEdge(compositeEdge, cost);
+                    }
+                    else
+                    {
+                    }
+                }
+            }
+
+            return reducedGraph;
+        }
+    }
+}
